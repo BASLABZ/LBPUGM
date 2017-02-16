f5432a5c33a28f5cf0e8a087b9171e7a5bc03df2
Ahmad Bastiar <nsjm.cs@gmail.com>
2017-02-16 13:29:35 +0700

    upload member area baru

---
+++ member/content-memberarea.php
@@ -0,0 +1,98 @@
+<div class="row  border-bottom navy-bg dashboard-header">
+           <div class="col-md-6">
+
+            <div class="profile-image">
+                <img src="../img/user.png" class="img-circle circle-border m-b-md" alt="profile">
+            </div>
+            <div class="profile-info">
+                <div class="">
+                    <div>
+                        <h2 class="no-margins">
+                            Ahmad Bastiar
+                        </h2>
+                        <h4>Member LBP</h4>
+                        <small>
+                           <b> Category member : | Instansi : | Fakultas :</b>
+                        </small>
+                    </div>
+                </div>
+            </div>
+    </div>
+   <div class="row">
+        <div class="col-sm-12">
+        <div class="row text-left">
+           <div class="col-lg-3 ">
+            <div class="widget style1 lazur-bg dim_about">
+                <div class="row">
+                    <div class="col-xs-4">
+                        <i class="fa fa-cloud fa-5x"></i>
+                    </div>
+                    <div class="col-xs-8 text-right">
+                        <span> Data Transaksi</span>
+                        <h3 class="font-bold">PEMINJAMAN</h3>
+                        <br>
+                        <a href="" class="btn btn-xs btn-info"> lihat data <span class="fa fa-arrow-right"></span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="widget style1 yellow-bg dim_about">
+                <div class="row">
+                    <div class="col-xs-4">
+                        <i class="fa fa-cloud fa-5x"></i>
+                    </div>
+                    <div class="col-xs-8 text-right">
+                        <span> Data Rekap </span>
+
+                        <h3 class="font-bold">PEMBAYARAN</h3>
+                        <br>
+                        <a href="" class="btn btn-xs btn-info"> lihat data <span class="fa fa-arrow-right"></span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="widget style1 lazur-bg dim_about">
+                <div class="row">
+                    <div class="col-xs-4">
+                        <i class="fa fa-cloud fa-5x"></i>
+                    </div>
+                    <div class="col-xs-8 text-right">
+                        <span> Informasi Data </span>
+
+                        <h3 class="font-bold">SALDO</h3>
+                        <br>
+                        <a href="" class="btn btn-xs btn-info"> lihat data <span class="fa fa-arrow-right"></span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+         <div class="col-lg-3">
+            <div class="widget style1 yellow-bg dim_about">
+                <div class="row">
+                    <div class="col-xs-4">
+                        <i class="fa fa-cloud fa-5x"></i>
+                    </div>
+                    <div class="col-xs-8 text-right">
+                        <span>Data Transaksi  </span>
+
+                        <h3 class="font-bold">PENGEMBALIAN</h3>
+                        <br>
+                        <a href="" class="btn btn-xs btn-info"> lihat data <span class="fa fa-arrow-right"></span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+   </div>
+</div>
+<div class="row  border-bottom yellow-bg dashboard-header ">
+    <div class="col-md-10">
+        <p style="font-family: arial;"><span class="fa fa-exclamation-triangle fa-2x"> SELAMAT DATANG DI LBP-UGM </span></p>
+    </div>
+    <div class="col-md-10">
+       
+    </div>
+</div>
---
+++ member/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>MEMBER AREA | LBPUGM</title>
+    <link href="../includes/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../includes/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="../includes/css/plugins/toastr/toastr.min.css" rel="stylesheet">
+    <link href="../includes/js/plugins/gritter/jquery.gritter.css" rel="stylesheet">
+    <link href="../includes/css/animate.css" rel="stylesheet">
+    <link href="../includes/css/style.css" rel="stylesheet">
+    <style type="text/css">
+        .dim_about {box-shadow: inset 0 0 0 rgba(30, 172, 174, 0.39), 0 10px 0 0 rgba(30, 172, 174, 0), 0 8px 10px rgba(123, 83, 83, 0.58);}
+    </style>
+</head>
+<body class="md-skin">
+    <div id="wrapper">
+        <?php include 'sidebar-memberarea.php'; ?>
+        <div id="page-wrapper" class="gray-bg dashbard-1">
+        <?php include 'topmenu-memberarea.php'; ?>
+        <!-- main members -->
+        <?php include 'content-memberarea.php'; ?>
+    </div>
+    <script src="../includes/js/jquery-2.1.1.js"></script>
+    <script src="../includes/js/bootstrap.min.js"></script>
+    <script src="../includes/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="../includes/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+    <script src="../includes/js/plugins/flot/jquery.flot.js"></script>
+    <script src="../includes/js/plugins/flot/jquery.flot.tooltip.min.js"></script>
+    <script src="../includes/js/plugins/flot/jquery.flot.spline.js"></script>
+    <script src="../includes/js/plugins/flot/jquery.flot.resize.js"></script>
+    <script src="../includes/js/plugins/flot/jquery.flot.pie.js"></script>
+    <script src="../includes/js/plugins/peity/jquery.peity.min.js"></script>
+    <script src="../includes/js/demo/peity-demo.js"></script>
+    <script src="../includes/js/inspinia.js"></script>
+    <script src="../includes/js/plugins/pace/pace.min.js"></script>
+    <script src="../includes/js/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <script src="../includes/js/plugins/gritter/jquery.gritter.min.js"></script>
+    <script src="../includes/js/plugins/sparkline/jquery.sparkline.min.js"></script>
+    <script src="../includes/js/demo/sparkline-demo.js"></script>
+    <script src="../includes/js/plugins/chartJs/Chart.min.js"></script>
+    <script src="../includes/js/plugins/toastr/toastr.min.js"></script>
+</body>
+</html>
