dae35ca76fae0812d387bdc0d31a85fc9e299dda
amandaaliasari <amanda.aliasari@gmail.com>
2017-02-18 22:07:35 +0700

    upload

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>LBP - UGM : HOME LBP UGM</title>
+    <title>LBP - UGM : HOME LBP UGM sdsdsdsdsdsdsd</title>
     <link href="includes/css/bootstrap.min.css" rel="stylesheet">
     <link href="includes/css/animate.css" rel="stylesheet">
     <link href="includes/font-awesome/css/font-awesome.min.css" rel="stylesheet">
