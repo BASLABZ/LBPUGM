5bf5a69830c4df140a7d140614e0ff82ee7b84b6
amandaaliasari <amanda.aliasari@gmail.com>
2017-02-24 13:35:05 +0700

    update manda sayang

--- member/pembayaran/rekap_pembayaran.php
+++ member/pembayaran/rekap_pembayaran.php
@@ -65,10 +65,20 @@
                                             <td><?php echo $rowRekap['payment_temp_info']; ?></td>
                                             <td><?php echo $rowRekap['loan_status']; ?></td>
                                             <td><?php echo $rowRekap['payment_status']; ?></td>
+                                            
                                             <td>
-                                                <a readonly href="index.php?hal=pembayaran/preview_rekappembayaran_perinvoice&id=<?php echo $rowRekap['loan_invoice']; ?>" class="btn btn-primary dim_about" target="_BLANK">
+                                               <?php 
+                                                    if ($rowRekap['payment_notif']=='VALID') {
+                                                        
+                                                ?>
+                                                 <a href="index.php?hal=pembayaran/preview_rekappembayaran_perinvoice&id=<?php echo $rowRekap['loan_invoice']; ?>" class="btn btn-primary dim_about" target="_BLANK">
                                                     <span class="fa fa-print"></span> Cetak
                                                 </a>
+                                                <?php }else if  ($rowRekap['payment_notif']=='TIDAK VALID'){
+                                                        echo "PEMBAYARAN ANDA TIDAK VALID ATAU TIDAK SESUAI, SILAHKAN CEK PEMBAYARAN,TAGIHAN/FILE POTO PEMBAYARAN ANDA
+                                                        <a href='#' class='btn btn-info dim_about'>Ubah Konfirmasi Pembayaran</a>
+                                                        ";
+                                                    } ?>
                                             </td>
                                         </tr>
                                  <?php } ?>
@@ -79,7 +89,7 @@
                     <hr>
                     <div class="row">
                         <div class="col-md-12">
-                        <h2>PEMBAYARAN DENDA</h2>
+                        <h2>PEMBAYARAN DENDA DENGAN SALDO</h2>
                         <table class="table table-responsive table-bordered table-striped table-hover"id="instrument">    
                         <thead>
                             <th>NO</th>
@@ -97,7 +107,55 @@
                             <tbody>
                                 <?php 
                                     $no = 0;
-                                    $query_rekap_pembayaran = mysql_query("SELECT * FROM trx_loan_application p JOIN trx_payment_temp t ON t.loan_app_id_fk = p.loan_app_id where t.payment_status = 'TANPA SALDO & MEMBAYAR DENDA' OR t.payment_status = 'SALDO & MEMBAYAR DENDA' AND p.member_id_fk = '".$_SESSION['member_id']."'");
+                                    $query_rekap_pembayaran = mysql_query("SELECT * FROM trx_loan_application p JOIN trx_payment_temp t ON t.loan_app_id_fk = p.loan_app_id where t.payment_status = 'SALDO & MEMBAYAR DENDA'  AND p.member_id_fk = '".$_SESSION['member_id']."'");
+                                   
+                                    while ($rowRekap = mysql_fetch_array($query_rekap_pembayaran)) {
+                                 ?>
+                                        <tr>
+                                            <td><?php echo ++$no; ?></td>
+                                            <td><?php echo $rowRekap['loan_invoice']; ?></td>
+                                            <td><?php echo $rowRekap['payment_temp_date']; ?></td>
+                                            <td><?php echo $rowRekap['payment_temp_confirm_date']; ?></td>
+                                            <td><?php echo $rowRekap['bankname']; ?></td>
+                                            <td><a href="../surat/<?php echo $rowRekap['payment_temp_photo']; ?>" class="label label-primary"><span class="fa fa-download"></span> BUKTI TRANSAFER</a></td>
+                                            <td>Rp. <?php echo $rowRekap['payment_temp_amount_transfer']; ?></td>
+                                            <td><?php echo $rowRekap['payment_temp_info']; ?></td>
+                                            <td><?php echo $rowRekap['loan_status']; ?></td>
+                                            <td><?php echo $rowRekap['payment_status']; ?></td>
+                                            <td>
+                                                <a href="index.php?hal=pembayaran/preview_rekappembayaran_perinvoice&id=<?php echo $rowRekap['loan_invoice']; ?>" class="btn btn-primary dim_about" target="_BLANK">
+                                                    <span class="fa fa-print"></span> Cetak
+                                                </a>
+                                            </td>
+                                        </tr>
+                                 <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    </div>
+                     <hr>
+                    <div class="row">
+                        <div class="col-md-12">
+                        <h2>PEMBAYARAN DENDA TANPA SALDO</h2>
+                        <table class="table table-responsive table-bordered table-striped table-hover"id="instrument">    
+                        <thead>
+                            <th>NO</th>
+                            <th>INVOICE</th>
+                            <th>TANGGAL INPUT</th>
+                            <th>TANGGAL KONFIRMASI</th>
+                            <th>NAMA BANK</th>
+                            <th>BUKTI TRANSFER</th>
+                            <th>JUMLAH</th>
+                            <th>KETERANGAN</th>
+                            <th>STATUS</th>
+                            <th>STATUS PEMBAYARAN</th>
+                            <th>CETAK</th>
+                        </thead>
+                            <tbody>
+                                <?php 
+                                    $no = 0;
+                                    $query_rekap_pembayaran = mysql_query("SELECT * FROM trx_loan_application p JOIN trx_payment_temp t ON t.loan_app_id_fk = p.loan_app_id where t.payment_status = 'TANPA SALDO & MEMBAYAR DENDA'  AND p.member_id_fk = '".$_SESSION['member_id']."'");
+                                   
                                     while ($rowRekap = mysql_fetch_array($query_rekap_pembayaran)) {
                                  ?>
                                         <tr>
--- menejemen/admin/pengembalian/detail_pengembalian.php
+++ menejemen/admin/pengembalian/detail_pengembalian.php
@@ -4,13 +4,14 @@
 			<th>NO</th>
 			<th>NAMA INSTRUMENT</th>
 			<th>JUMLAH</th>
+			<th>AKSI</th>
 		</thead>
 		<tbody>
 <?php 
 			include '../../inc/inc-db.php';
 		$invoice = $_POST['id'];
 		
-		$sqldetail = mysql_query("SELECT * FROM trx_loan_application_detail d join ref_instrument i on d.instrument_id_fk = i.instrument_id join trx_loan_application a on d.loan_app_id_fk = a.loan_app_id where a.loan_invoice = '".$invoice."'");
+		$sqldetail = mysql_query("SELECT * FROM trx_loan_application_detail d join ref_instrument i on d.instrument_id_fk = i.instrument_id join trx_loan_application a on d.loan_app_id_fk = a.loan_app_id where  a.loan_invoice = '".$invoice."'");
 		$no =1;
 		while ($rowDetailPeminjaman = mysql_fetch_array($sqldetail)) {
 			$status = $rowDetailPeminjaman['loan_status_detail'];
@@ -19,17 +20,17 @@
 				<td><?php echo $no++; ?></td>
 				<td><?php echo $rowDetailPeminjaman['instrument_name']; ?></td>
 				<td><?php echo $rowDetailPeminjaman['loan_amount']; ?></td>
+				<td>
+					<form class="role" method="POST" action="">
+						<div class="form-group">
+							<input type="hidden" name="intrument_quantity_temp" value="<?php echo $rowDetailPeminjaman['intrument_quantity_temp']; ?>">
+							<input type="hidden" name="instrument_quantity" value="<?php echo $rowDetailPeminjaman['instrument_quantity']; ?>">
+							<button type="submit" class="btn btn-info"> <span class="fa fa-check"></span> Kembalikan</button>
+						</div>
+					</form>
+				</td>
 			</tr>
 <?php } ?>
 		</tbody>
 	</table>
-	<div class="row">
-		<div class="col-md-12">
-			<form class="role" method="POST" action="index.php?hal=pengembalian/proses_pengembalian_alat">
-				<div class="form-group">
-					<input type="hidden" name="invoice" value="<?php echo $invoice; ?>">
-					<button type="submit" class="btn btn-block btn-info dim_about"> <span class="fa fa-check"></span> KONFIRMASI PENGEMBALIAN ALAT</button>
-				</div>
-			</form>
-		</div>
-	</div>
+	
